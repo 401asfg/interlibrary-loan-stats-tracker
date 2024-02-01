8cabd3bffd2c58a30ac6fb2c44fabaa7fe8a1f17
Michael Allan <mallan@vcc.ca>
2024-01-31 17:14:26 -0800

    Added major headers of the form

---
+++ resources/views/form.blade.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <h1>ILL Statistics Form</h1>
+    <h2>Request Fulfilled</h2>
+    <h2>Request Info</h2>
+    <h2>Parties Involved</h2>
+</html>
