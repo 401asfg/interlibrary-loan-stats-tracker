1943f9600c83b98551ad39f86d01838579c1e283
Michael Allan <mallan@vcc.ca>
2024-05-14 16:52:08 -0700

    Added tests for pressing the cancel button on the form after all the fields have been filled out

--- tests/Browser/Pages/Form/FormCancelTest.php
+++ tests/Browser/Pages/Form/FormCancelTest.php
@@ -11,6 +11,20 @@ use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Carbon\Carbon;
 
+class FormCancelTestBrowser extends Browser
+{
+    public function fillout()
+    {
+        $this->click('@resource_book')
+            ->click('@action_borrow')
+            ->type('@requestor_notes', 'notes')
+            ->click('@searchable_select_input')
+            ->type('@searchable_select_input', 'british')
+            ->click('@searchable_select_result_0')
+            ->click('@vcc_borrower_type_student');
+    }
+}
+
 class FormCancelTest extends DuskTestCase
 {
     public function testCancelFromIndex()
@@ -18,6 +32,17 @@ class FormCancelTest extends DuskTestCase
         $this->browse(function (Browser $browser) {
             $browser->click('@submit')
                 ->waitFor('@form_title')
+                ->click('@cancel')
+                ->assertVisible('@index_title');
+        });
+    }
+
+    public function testCancelFromIndexFilledOut()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->click('@submit')
+                ->waitFor('@form_title')
+                ->fillout()
                 ->click('@cancel')
                 ->assertVisible('@index_title');
         });
@@ -47,6 +72,31 @@ class FormCancelTest extends DuskTestCase
         });
     }
 
+    public function testCancelAfterSubmittingAnotherFillout()
+    {
+        $illRequest = ILLRequest::create([
+            'request_date' => Carbon::today()->toDateString(),
+            'fulfilled' => 'false',
+            'unfulfilled_reason' => 'reason',
+            'resource' => ILLRequest::RESOURCES['book'],
+            'action' => ILLRequest::ACTIONS['borrow'],
+            'library_id' => 58,
+            'vcc_borrower_type' => ILLRequest::VCC_BORROWER_TYPES['student'],
+            'requestor_notes' => 'notes'
+        ]);
+
+        $illRequest->save();
+
+        $this->browse(function (Browser $browser) {
+            $browser->visit('ill-requests/1')
+                ->click('@submit')
+                ->waitFor('@form_title')
+                ->fillout()
+                ->click('@cancel')
+                ->assertVisible('@index_title');
+        });
+    }
+
     public function testCancelAfterClickingEdit()
     {
         $illRequest = ILLRequest::create([
@@ -71,6 +121,31 @@ class FormCancelTest extends DuskTestCase
         });
     }
 
+    public function testCancelAfterClickingEditFillout()
+    {
+        $illRequest = ILLRequest::create([
+            'request_date' => Carbon::today()->toDateString(),
+            'fulfilled' => 'false',
+            'unfulfilled_reason' => 'reason',
+            'resource' => ILLRequest::RESOURCES['book'],
+            'action' => ILLRequest::ACTIONS['borrow'],
+            'library_id' => 58,
+            'vcc_borrower_type' => ILLRequest::VCC_BORROWER_TYPES['student'],
+            'requestor_notes' => 'notes'
+        ]);
+
+        $illRequest->save();
+
+        $this->browse(function (Browser $browser) {
+            $browser->visit('ill-requests/1')
+                ->click('@edit')
+                ->waitFor('@form_title')
+                ->fillout()
+                ->click('@cancel')
+                ->assertVisible('@submission_title');
+        });
+    }
+
     public function testCancelAfterClickingDelete()
     {
         $illRequest = ILLRequest::create([
@@ -90,6 +165,31 @@ class FormCancelTest extends DuskTestCase
             $browser->visit('ill-requests/1')
                 ->click('@delete')
                 ->waitFor('@form_title')
+                ->click('@cancel')
+                ->assertVisible('@index_title');
+        });
+    }
+
+    public function testCancelAfterClickingDeleteFillout()
+    {
+        $illRequest = ILLRequest::create([
+            'request_date' => Carbon::today()->toDateString(),
+            'fulfilled' => 'false',
+            'unfulfilled_reason' => 'reason',
+            'resource' => ILLRequest::RESOURCES['book'],
+            'action' => ILLRequest::ACTIONS['borrow'],
+            'library_id' => 58,
+            'vcc_borrower_type' => ILLRequest::VCC_BORROWER_TYPES['student'],
+            'requestor_notes' => 'notes'
+        ]);
+
+        $illRequest->save();
+
+        $this->browse(function (Browser $browser) {
+            $browser->visit('ill-requests/1')
+                ->click('@delete')
+                ->waitFor('@form_title')
+                ->fillout()
                 ->click('@cancel')
                 ->assertVisible('@index_title');
         });
